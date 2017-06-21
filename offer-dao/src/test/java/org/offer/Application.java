455913ae1b1ec9035040326dd7c0037caa02712c
673132594@qq.com <673132594@qq.com>
2017-06-21 15:10:32 +0800

    新增工程包

---
+++ offer-dao/src/test/java/org/offer/Application.java
@@ -0,0 +1,18 @@
+package org.offer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * 
+ * <p>Description: [描述该类概要功能介绍]</p>
+ * Created on 2017年5月16日
+ * @author  <a href="mailto: hanshixiong@camelotchina.com">韩士雄</a>
+ * @version 1.0 
+ * Copyright (c) 2017 北京柯莱特科技有限公司 交付部
+ */
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run( Application.class, args);
+	}
+}
