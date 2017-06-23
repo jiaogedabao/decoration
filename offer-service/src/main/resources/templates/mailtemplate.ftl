1de492f8f680a22b363d8a432c9a9d519912ed84
673132594@qq.com <673132594@qq.com>
2017-06-23 15:26:31 +0800

    添加测试邮箱服务文件

---
+++ offer-service/src/main/resources/templates/mailtemplate.ftl
@@ -0,0 +1,48 @@
+<!DOCTYPE head PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">  
+<html>  
+<head>  
+<title></title>  
+<meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=utf-8">  
+<style type="text/css">  
+body {  
+    font-size: 14px;  
+    line-height: 1.5  
+}  
+</style>  
+</head>  
+<body>  
+    <table cellspacing="0" cellpadding="20">  
+        <tr>  
+            <td>  
+                <table width="500" cellspacing="0" cellpadding="1">  
+                    <tr>  
+                        <td bgcolor="#FF8E00" align="left"  
+                            style="font-family:'lucida grande',tahoma,'bitstream vera sans',helvetica,sans-serif;line-height:150%;color:#FFF;font-size:24px;font-weight:bold;padding:4px">   
+                            Home-decoration
+                        <th></th>  
+                    </tr>  
+                    <tr>  
+                        <td bgcolor="#FF8E00">  
+                            <table width="100%" cellspacing="0" bgcolor="#FFFFFF"  
+                                cellpadding="20">  
+                                <tr>  
+                                    <td  
+                                        style="font-family:'lucida grande',tahoma,'bitstream vera sans',helvetica,sans-serif;line-height:150%;color:#000;font-size:14px;">  
+                                        亲爱的${customer}：  
+                                        <blockquote>  
+                                            <br> <strong>${userName}</strong>为您发的报价单在邮件的附件中，请查收！<br>  
+                                            <br><strong>${userName}</strong>联系方式：<br>
+                                            	Email:<strong>${userEmail}</strong><br>
+                                            	phone:<strong>${userPhone}</strong><br>  
+                                            <br>  
+                                        </blockquote> <br> <br>Home-decoration 社区<br>  
+                                        此邮件为<strong>Home-decoration</strong>系统自动发出，请勿回复。</td>
+                                </tr>  
+                            </table></td>  
+                    </tr>  
+                </table></td>  
+        </tr>  
+    </table>  
+</body>
+</html>  
+
---
+++ offer-service/src/test/java/org/offer/service/test/EmailTest.java
@@ -0,0 +1,75 @@
+package org.offer.service.test;
+
+import java.io.File;
+import java.util.Map;
+
+import javax.mail.internet.MimeMessage;
+
+import org.apache.commons.collections.map.HashedMap;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+
+import freemarker.template.Template;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmailTest {
+
+	@Autowired
+    private JavaMailSender mailSender; //自动注入的Bean
+	
+	@Value("${spring.mail.username}")
+    private String Sender; //读取配置文件中的参数
+	
+	@Autowired
+    private FreeMarkerConfigurer freeMarkerConfigurer;
+	
+	@Test
+    public void sendSimpleMail() throws Exception {
+		SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("wlsnnew@sina.com");
+        message.setTo("673132594@qq.com");
+        message.setSubject("主题：简单邮件");
+        message.setText("测试邮件内容");
+        mailSender.send(message);
+    }
+	
+	@Test
+	public void sendTempalte(){
+		MimeMessage message = null;
+        try {
+            message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom("wlsnnew@sina.com");
+            helper.setTo("1099857577@qq.com");
+            helper.setSubject("主题：Home-Decoratioin 报价单");
+            Map<String, Object> model = new HashedMap();
+            model.put("userName", "wangdada");
+            model.put("customer", "小童");
+            model.put("userEmail", "wangdada.qq.com");
+            model.put("userPhone", "110119120112");
+            //读取 html 模板
+            Template template = freeMarkerConfigurer.getConfiguration().getTemplate("/mailtemplate.ftl");
+            String html = FreeMarkerTemplateUtils.processTemplateIntoString(template, model);
+            helper.setText(html, true);
+          //注意项目路径问题，自动补用项目路径
+            FileSystemResource file = new FileSystemResource(new File("src/main/resources/baojiadan.xls"));
+            //加入邮件
+            helper.addAttachment("报价单", file);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        mailSender.send(message);
+		
+	}
+}
