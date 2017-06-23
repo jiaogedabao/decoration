1d70e424a8577f6b7e8367276d51a1bd0dc4f84b
673132594@qq.com <673132594@qq.com>
2017-06-23 15:25:46 +0800

    添加测试邮箱服务

--- offer-dao/src/test/java/org/offer/dao/test/UserDaoTest.java
+++ offer-dao/src/test/java/org/offer/dao/test/UserDaoTest.java
@@ -5,7 +5,9 @@ import javax.annotation.Resource;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.offer.bean.UserinfoBean;
+import org.offer.dao.DicProjectDAO;
 import org.offer.dao.UserinfoDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -16,10 +18,13 @@ public class UserDaoTest {
 	@Resource
 	private UserinfoDAO dao;
 	
+	@Autowired
+    private DicProjectDAO  dicProjectDAO ;
+	
 	@Test
 	public void add(){
 		UserinfoBean user = new UserinfoBean();
-		user.setLoginName("abcd");
+		user.setLoginName("ggggg");
 		dao.add(user);
 	}
 }
